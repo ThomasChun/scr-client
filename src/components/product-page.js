473a2275c62ad9ef4f76f80d13950d3ed6635928
Thomas Chun <tchun2055@gmail.com>
2019-02-13 16:52:26 -0800

    added basic layout for dynamic product details pages

--- src/components/product-page.js
+++ src/components/product-page.js
@@ -4,36 +4,12 @@ import requiresLogin from './requires-login';
 import NavBar from './nav-bar';
 import RightBar from './right-bar';
 import { fetchProducts } from '../actions/products';
-import { Link } from 'react-router-dom';
 
 export class ProductPage extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchProducts());
   }
   render() {
-
-    // const { products } = this.props;
-
-    // if (!products) {
-    //     return null;
-    // }
-
-    // let baseball2019;
-
-    // if (products !== null) {
-    //     baseball2019 = products.filter(product => product.sport === 'Baseball' && product.year === '2019');
-    //     baseball2019 = baseball2019.map((product, index) => {
-    //         return (
-    //             <div className="product-by-year-info-container" key={index} id={product.id}>
-    //                 <img src={product.productImageUrl} alt="2019 Topps Baseball" />
-    //                 <ul>
-    //                     <Link className="product-by-year-name" to={{ pathname: `/baseball-2019/${product.name}`, state: {product: product}}}><li><b>{product.name}</b></li></Link>
-    //                     <li className="product-by-year-release-date"><b>Release Date:</b> {product.releaseDate}</li>
-    //                 </ul>
-    //             </div>
-    //         )
-    //     })
-    // }
 
     const { product } = this.props.location.state;
     console.log(product);
